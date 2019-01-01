0099a903c7e10096b897938fcb5311f02689681e
born2lie <byunghun.ee@gmail.com>
2019-01-01 01:25:00 -0500

    POST /api/events/ return 201

--- src/main/java/com/toto/rest/springrest/events/Event.java
+++ src/main/java/com/toto/rest/springrest/events/Event.java
@@ -2,13 +2,19 @@ package com.toto.rest.springrest.events;
 
 import lombok.*;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Setter @Getter @EqualsAndHashCode(of = "id")
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
+
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDataTime;
@@ -21,6 +27,8 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+
+    @Enumerated(EnumType.STRING)
     private EventStats eventStats;
 
 
---
+++ src/main/java/com/toto/rest/springrest/events/EventRepository.java
@@ -0,0 +1,6 @@
+package com.toto.rest.springrest.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+}
